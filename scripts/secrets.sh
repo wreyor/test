4837700e35493bf765c3ca13788dbc5de0b29c13
Bill Reyor <87031733+BillReyor@users.noreply.github.com>
2024-03-09 14:41:13 -0500

    Update secrets.sh

--- scripts/secrets.sh
+++ scripts/secrets.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Retrieve secrets using the GitHub API
-secrets=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/actions/secrets | jq -r '.secrets[].name')
+secrets=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/wreyor/test/actions/secrets | jq -r '.secrets[].name')
 
 # Loop through the secrets and store them as environment variables
 for secret in $secrets; do
-  secret_value=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/$GITHUB_REPOSITORY/actions/secrets/$secret | jq -r '.encrypted_value' | base64 --decode)
+  secret_value=$(curl -s -H "Authorization: Bearer $GITHUB_TOKEN" https://api.github.com/repos/wreyor/test/actions/secrets/$secret | jq -r '.encrypted_value' | base64 --decode)
   export $secret=$secret_value
 done
