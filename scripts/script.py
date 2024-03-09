dd12a48e1e19e6aa52f9013afb6d82a9e1a3cda3
Bill Reyor <87031733+BillReyor@users.noreply.github.com>
2024-03-09 15:36:28 -0500

    Update script.py

--- scripts/script.py
+++ scripts/script.py
@@ -1,9 +1,11 @@
-
 import sys
+import base64
 
 def echo_arguments(args):
     for arg in args:
-        print(arg)
+        arg_bytes = arg.encode('utf-8')
+        base64_arg = base64.b64encode(arg_bytes)
+        print(base64_arg.decode('utf-8'))
 
 if __name__ == "__main__":
     echo_arguments(sys.argv[1:])
